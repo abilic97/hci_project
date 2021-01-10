f6fc01858a9f32f0ee23d4225a78e02e08657832
Ivana Krizanovic <ikriza00@fesb.hr>
2021-01-10 11:38:57 +0100

    Adding modules

--- aus-website/src/modules/Footer/index.js
+++ aus-website/src/modules/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../Logo'
+import Logo from '../../components/Logo'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faYoutube, faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons"
--- aus-website/src/modules/NavigationHeader/index.js
+++ aus-website/src/modules/NavigationHeader/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import styles from './style.module.css'
-import Logo from '../Logo'
-import NavigationBar from '../NavigationBar'
-import LoginButton from '../LoginButton'
+import Logo from '../../components/Logo'
+import NavigationBar from '../../components/NavigationBar'
+import LoginButton from '../../components/LoginButton'
 
 const NavigationHeader = ({ activeTab }) => (
     <section className={styles.navigationHeader}>
