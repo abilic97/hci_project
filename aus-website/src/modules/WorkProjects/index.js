f1d2e274bfa5ae525a2635bc2564fdabb6fdbae5
Ivana Krizanovic <ikriza00@fesb.hr>
2021-02-11 17:14:30 +0100

    Added Work Projects components

---
+++ aus-website/src/modules/WorkProjects/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ContactHeaderImage from '../../components/Images/ContactHeaderImage/ContactHeaderImage'
+import ProjectsMenu from '../../components/ProjectsMenu'
+
+import styles from './style.module.css'
+
+const WorkProjects = () => (
+    <section>
+        <div className={styles.workProjectsTitle}>Projects</div>
+        <div>
+        <ProjectsMenu >/</ProjectsMenu>
+        </div>
+        
+        
+
+                <div className={styles.projectContent}>
+                    Flow Design Team started VECTOR project as a group of enthusiasts at University of Split - FESB. After thorough research, our team members enrolled in undergraduate and graduate engineering programs soon started to develop and design the first concept.
+<p> </p>
+Our mission and goal are to share our interest and passion with fellow students, teachers and community – to present the necessary hands-on engineering skills involved in building aerial robotic systems.
+<p></p>
+Student organisation based at University of Split.
+We develop and prototype UAVs - write our own code, test electronic systems and design our own aerial vehicle.
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos do
+<ContactHeaderImage></ContactHeaderImage>
+                    <p>
+                        We develop and prototype UAVs - write our own code, test electronic systems and design our own aerial vehicle.
+                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+At vero eos et accusamus et iusto  </p>
+                </div>
+
+    </section>
+)
+
+export default WorkProjects
