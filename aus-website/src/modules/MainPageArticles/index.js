4965d6ab64c4c4d49505ac78e9397cc41961d719
Andjela Bilic <andjela.bilic97@gmail.com>
2021-02-17 16:46:40 +0100

    Connected Main Page to Latest Blog Posts

--- aus-website/src/modules/MainPageArticles/index.js
+++ aus-website/src/modules/MainPageArticles/index.js
@@ -1,17 +1,75 @@
 import React from 'react'
-
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import { renderRichText } from "gatsby-source-contentful/rich-text"
+import Img from 'gatsby-image'
+import TextEllipsis from 'react-text-ellipsis';
 import styles from './style.module.css'
 
-const MainPageArticles = () => (
-    <section>
-      <div className={styles.articlesTitle}>Latest from blog</div>
+const MainPageArticles = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(limit: 3) {
+        edges {
+          node {
+            title
+            blogImage {
+              fixed (width: 500 height:200) {
+                srcWebp
+                srcSetWebp
+                srcSet
+                src
+                height
+                base64
+                width
+                aspectRatio
+              }
+            }
+            content {
+              raw
+            }
+            slug
+          }
+        }
+      }
+  }
+`)
+  return (
+    <div>
+      <div className={styles.articlesTitle}>Featured Blog Posts</div>
       <div className={styles.articleContainer}>
-          <div className={styles.article}></div>
-          <div className={styles.article}></div>
-          <div className={styles.article}></div>
+        {data.allContentfulBlogPost.edges.map(node => {
+          return (
+            <div className={styles.article}>
+              <div>
+                <Img fixed={node.node.blogImage.fixed} />
+              </div>
+              <div className={styles.articleTitle}>{node.node.title}</div>
+              <div className={styles.elipsis}>
+                <TextEllipsis
+                  lines={5}
+                  tag={'p'}
+                  ellipsisChars={'...'}
+                  tagClass={'className'}
+                  debounceTimeoutOnResize={200}
+                  useJsOnly={true}
+                  onResult={(result) => {
+                    if (result === TextEllipsis.RESULT.TRUNCATED)
+                      console.log('text get truncated');
+                    else
+                      console.log('text does not get truncated');
+                  }}>
+                  {renderRichText(node.node.content)}
+                </TextEllipsis>
+              </div>
+              <Link className={styles.readMore} to={`/posts/${node.node.slug}`}>
+                <div className={styles.linkToPost}>Continue reading</div>
+              </Link>
+            </div>
+          )
+        })}
       </div>
-    </section>
+    </div>
   )
-//TODO: Connect with blogs page when blogs is finished
+}
 
 export default MainPageArticles
